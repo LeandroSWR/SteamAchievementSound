daabc508b00716e9c2dff53eb30c3de64bb94d11
Leandro Brás <shadowgaming.7@gmail.com>
2021-12-17 16:27:53 +0000

    Add sounds

---
+++ SteamAchievementSound/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAchievementSound
+{
+    class Program
+    {
+        private static AchievementSound achievementSound;
+
+        static void Main(string[] args)
+        {
+            achievementSound = new AchievementSound();
+
+
+        }
+    }
+}
